b51f56fa4328f91238b3a2b0dc3ac2d7645bcbbc
demo <demo@copper.local>
2024-01-01 23:48:49 +0530

    somewhat working solution

--- Solver.java
+++ Solver.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -28,15 +29,19 @@ public class Solver {
         int steps = node.cost - node.current.manhattan();
         for (Board neighbor : node.current.neighbors()) {
             int distance = neighbor.manhattan();
-            if (distance == 0) {
-                this.solvable = !isTwin;
-                return true;
-            }
+            // StdOut.println("distance: " + distance + " steps: " + steps);
             if (neighbor == node.previous) {
                 continue;
             }
+
             int cost = steps + 1 + distance;
             q.insert(new Node(neighbor, node.current, cost));
+
+            if (distance == 0) {
+                // StdOut.println("solved: " + neighbor + " from: " + node.current);
+                this.solvable = !isTwin;
+                return true;
+            }
 
         }
         return false;
@@ -44,13 +49,27 @@ public class Solver {
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        List<Node> history = new ArrayList<Node>();
+
         q1.insert(new Node(initial, null, initial.manhattan()));
         Board twin = initial.twin();
         q2.insert(new Node(twin, null, twin.manhattan()));
 
         while (!q1.isEmpty() && !q2.isEmpty()) {
             Node node1 = q1.delMin();
+            // if (history.size() > 100) {
+            //     StdOut.println("exiting");
+            //     System.exit(0);
+            // }
+            history.add(node1);
+            if (node1.cost == 0) {
+                this.solvable = true;
+                break;
+            }
+            // StdOut.println("history length: " + history.size());
+
             if (simulate(node1, q1, false)) {
+                history.add(q1.min());
                 break;
             }
 
@@ -58,6 +77,36 @@ public class Solver {
             if (simulate(node2, q2, true)) {
                 break;
             }
+            // StdOut.println("q1 length: " + q1.size());
+
+            // StdOut.println("q2 length: " + q2.size());
+        }
+
+        if (this.solvable) {
+            // process
+            Node lastElement = null;
+            for (int i = history.size() - 1; i >= 0; i--) {
+                Node element = history.get(i);
+                if (lastElement == null) {
+                    lastElement = element;
+                    this.solution.add(element.current);
+                    // StdOut.println("state: " + element.current.manhattan() + " " + element.current);
+                    continue;
+                }
+                if (lastElement.previous == element.current) {
+                    this.solution.add(element.current);
+                    // StdOut.println("state: " + element.current.manhattan() + " " + element.current);
+                }
+                lastElement = element;
+            }
+            List<Board> solution2 = new ArrayList<Board>();
+            for (int i = solution.size() - 1; i >= 0; i--) {
+                solution2.add(solution.get(i));
+            }
+            this.solution = solution2;
+        }
+        else {
+            history.clear();
         }
     }
 
@@ -68,37 +117,84 @@ public class Solver {
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        return 0;
+        return this.solution.size() - 1;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return new ArrayList<Board>();
+        return this.solution;
     }
 
-    // test client (see below)
+
     public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+
+        /*
+
         {
             int[][] tiles = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
             Board board = new Board(tiles);
             Solver solver = new Solver(board);
             StdOut.println("solvable?: " + solver.isSolvable());
+            StdOut.println("moves?: " + solver.moves());
+            StdOut.println("solution?: " + solver.solution());
         }
+        StdOut.println("------------------------------");
 
         {
-            int[][] tiles = { { 1, 2, 3 }, { 4, 5, 6 }, { 8, 7, 0 } };
+            int[][] tiles = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 0, 8 } };
             Board board = new Board(tiles);
             Solver solver = new Solver(board);
             StdOut.println("solvable?: " + solver.isSolvable());
+            StdOut.println("moves?: " + solver.moves());
+            StdOut.println("solution?: " + solver.solution());
         }
-
+        StdOut.println("------------------------------");
 
         {
-            int[][] tiles = { { 1, 2, 3 }, { 4, 6, 5 }, { 8, 7, 0 } };
+            int[][] tiles = { { 1, 2, 3 }, { 4, 8, 6 }, { 7, 0, 5 } };
             Board board = new Board(tiles);
             Solver solver = new Solver(board);
             StdOut.println("solvable?: " + solver.isSolvable());
+            StdOut.println("moves?: " + solver.moves());
+            StdOut.println("solution?: " + solver.solution());
+
         }
+
+        StdOut.println("------------------------------");
+
+
+        {
+            int[][] tiles = { { 0, 1, 3 }, { 4, 2, 5 }, { 7, 8, 6 } };
+            Board board = new Board(tiles);
+            Solver solver = new Solver(board);
+            StdOut.println("solvable?: " + solver.isSolvable());
+            StdOut.println("moves?: " + solver.moves());
+            StdOut.println("solution?: " + solver.solution());
+
+        }
+
+     */
 
 
     }
